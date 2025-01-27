3574e06ebf7fcc4d684a320eca0e96eacf577d03
AvelaNikani <avelarnikani47@gmail.com>
2025-01-27 14:49:50 +0200

    update

--- indexExercise2.js
+++ indexExercise2.js
@@ -1,8 +1,11 @@
 import express from "express"
-
+// 1.Make sure to use the following dependencies within your app:
+// a. express
+// b. nodemon(as a dev dependency, if you know the alternative then you don’t have to
+// install it 😉)
 const PicknSteal = express()
 const PORT = process.env.PORT || 3002
-
+// 2. Create GET routes for the employees and the managers.
 PicknSteal.get('/employees',(req,res)=>{
     res.json({
         message: "We have to get the employees to the path."
@@ -13,6 +16,8 @@ PicknSteal.get('/managers',(req,res)=>{
         message: "We have to get the managers to the path."
     })
 })
+// 3. Create routes for the POST, PATCH and DELETE methods. Make sure they are clearly
+// shown and explained.
 PicknSteal.post('/employees',(req,res)=>{
     res.json({
         message: "We need to post the employees on the management systems."
